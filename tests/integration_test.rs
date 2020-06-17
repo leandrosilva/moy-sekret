c571e3adadffd829031c877a24e7d40d773fc479
Leandro Silva <leandrodoze@gmail.com>
2020-06-17 11:39:01 -0300

    integration test for --override on init

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,6 +8,7 @@ use std::path::Path;
 
 const F_KEYS_DIR: &str = "./test_keys";
 const F_PROFILE: &str = "tester";
+const F_OVERRIDE_PROFILE: bool = false;
 
 // Helpers
 //
@@ -59,17 +60,18 @@ fn should_init_a_profile_and_save_them_to_a_given_directory() {
         let keys_dir = F_KEYS_DIR.to_string();
         let profile = F_PROFILE.to_string();
 
-        match init(&keys_dir, &profile, true) {
-            Ok(_) => {
-                match profile_exists(&keys_dir, &profile) {
+        match init(&keys_dir, &profile, F_OVERRIDE_PROFILE) {
+            Ok(_) => match profile_exists(&keys_dir, &profile) {
                 Ok(_) => assert!(true),
                 Err(reason) => assert!(
                     false,
-                    format!("Should exist a key pair for {} in {} dir but: {}", &profile, &keys_dir, reason)
-                )
-            }
+                    format!(
+                        "Should exist profile {} in {} directory but: {}",
+                        &profile, &keys_dir, reason
+                    )
+                ),
             },
-            Err(e) => assert!(false, format!("Should have created but: {}", e)),
+            Err(e) => assert!(false, format!("Should have initiated but: {}", e)),
         }
     })
 }
@@ -86,21 +88,65 @@ fn should_not_init_due_to_permission_denied_on_keys_directory() {
         let keys_dir = String::from("/keys");
         let profile = F_PROFILE.to_string();
 
-        match init(&keys_dir, &profile, true) {
-            Ok(_) => assert!(false, "Should not create key pair"),
-            Err(e) => {
+        match init(&keys_dir, &profile, F_OVERRIDE_PROFILE) {
+            Ok(_) => assert!(false, "Should have not initiated"),
+            Err(reason) => {
                 assert_eq!(
                     "Initialization failed while creating key pair: Could not create keys directory: Failed to create keys' directory: Permission denied (os error 13)",
-                    e.to_string()
+                    reason.to_string()
                 );
                 match profile_exists(&keys_dir, &profile) {
                     Ok(_) => assert!(
                         false,
-                        format!("Should not exist any key file for {} in {} dir", &profile, &keys_dir)
+                        format!(
+                            "Should not exist profile {} in {} directory",
+                            &profile, &keys_dir
+                        )
                     ),
                     Err(_) => assert!(true),
                 };
             }
+        }
+    })
+}
+
+#[test]
+fn should_init_when_profile_exists_and_override_flag_is_present() {
+    run_test(|| {
+        let keys_dir = F_KEYS_DIR.to_string();
+        let profile = F_PROFILE.to_string();
+
+        match init(&keys_dir, &profile, F_OVERRIDE_PROFILE) {
+            Ok(_) => {
+                let flag_override_profile = true;
+                match init(&keys_dir, &profile, flag_override_profile) {
+                    Ok(_) => assert!(true),
+                    Err(reason) => assert_eq!("Should have initiated and overridden existent profile but:", reason.to_string()),
+                }
+            }
+            Err(e) => assert!(false, format!("Should have initiated profile but: {}", e)),
+        }
+    })
+}
+
+#[test]
+fn should_not_init_when_profile_exists_and_override_flag_is_not_present() {
+    run_test(|| {
+        let keys_dir = F_KEYS_DIR.to_string();
+        let profile = F_PROFILE.to_string();
+
+        match init(&keys_dir, &profile, F_OVERRIDE_PROFILE) {
+            Ok(_) => {
+                let flag_override_profile = false;
+                match init(&keys_dir, &profile, flag_override_profile) {
+                    Ok(_) => assert!(
+                        false,
+                        "Should not initialize an existent profile when override flag is not present"
+                    ),
+                    Err(reason) => assert_eq!("Initialization failed because profile already exists", reason.to_string()),
+                }
+            }
+            Err(e) => assert!(false, format!("Should have initiated profile but: {}", e)),
         }
     })
 }
